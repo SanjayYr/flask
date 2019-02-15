7dce914b0b222bd0868eb3da8469f32d9c54823b
Sanjay <yella016@github.umn.edu>
2019-02-14 19:41:31 -0600

    arguments and for loop

--- flaskblog.py
+++ flaskblog.py
@@ -2,10 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'SanjayYr',
+        'title': 'BlogPost 1',
+        'content': 'First Post Content',
+        'date': 'February 14, 2019'
+    },
+    {
+        'author': 'SanjayYr2',
+        'title': 'BlogPost 2',
+        'content': 'Second Post Content',
+        'date': 'February 15, 2019'
+    }
+]
+
 @app.route("/")
 @app.route("/home")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
